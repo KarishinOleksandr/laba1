9b22c8d70a7ae27cf548a81d8e86d3cb387cd84e
KarishinOleksandr <128784285+KarishinOleksandr@users.noreply.github.com>
2023-05-15 12:41:25 +0300

    siogodninemohnaa

---
+++ 16/scrit.js
@@ -0,0 +1,6 @@
+'use strict'
+let elem = document.getElementById("img1");
+
+let funct1 = document.getElementById("img2");
+    let fnt1 = function(){alert(1)}
+funct1.addEventListener ('click', fnt1 )
